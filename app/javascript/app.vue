3b70a08dae68ecb75bdc96e54201482a05cc993f
Rafael Bravo <antoniorafael97@hotmail.com>
2020-11-24 11:46:23 -0600

    Add function to submit task with post

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -3,23 +3,50 @@
     <p>{{ original_board.name }}</p>
     <p>{{ original_board }}</p>
 
-    <div v-for="list in original_board.lists" :key="list.id" class="col-3">
+    <div v-for="list in board.lists" :key="list.id" class="col-3">
       <h6>{{ list.name }}</h6>
       <hr>
 
       <div v-for="task in list.tasks" :key="task.id" class="card card-body">
         {{ task.name }}
       </div>
+      <div class="card card-body">
+        <textarea v-model="messages[list.id]" class="form-control"></textarea>
+        <button @click="submitTask(list.id)" class="btn btn-secondary">Add</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Rails from '@rails/ujs';
 export default {
   props: ['original_board'],
   data: function () {
     return {
-      message: "Hello Vue!"
+      messages: {},
+      board: this.original_board
+    }
+  },
+
+  methods: {
+    submitTask: function(list_id) {
+      console.log(this.messages[list_id])
+      var data = new FormData
+      data.append("task[list_id]", list_id)
+      data.append("task[name]", this.messages[list_id])
+
+      Rails.ajax({
+        url: "/tasks",
+        type: "POST",
+        data: data,
+        dataType: "json",
+        success: data => {
+          const index = this.board.lists.findIndex(item => item.id === list_id)
+          this.board.lists[index].tasks.push(data)
+          this.messages[list_id] = undefined
+        }
+      })
     }
   }
 }
